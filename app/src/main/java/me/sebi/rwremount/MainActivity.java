79ccc6f3ff3825eca59ee080ad8faea30da9a44e
Sebastian <SebiderSushi@t-online.de>
2020-03-17 01:15:52 +0100

    Show some text in the activity

--- app/src/main/java/me/sebi/rwremount/MainActivity.java
+++ app/src/main/java/me/sebi/rwremount/MainActivity.java
@@ -2,10 +2,16 @@ package me.sebi.rwremount;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LinearLayout.LayoutParams mainLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        TextView text = new TextView(this);
+        text.setText("All set!");
+        setContentView(text, mainLayoutParams);
     }
 }
