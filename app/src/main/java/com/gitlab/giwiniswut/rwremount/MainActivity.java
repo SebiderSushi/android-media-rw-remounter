0096c33fba80755cbcec3868836d9cb58f523e1e
Sebastian <SebiderSushi@t-online.de>
2020-05-15 08:43:46 +0200

    Add a way to request root access in the UI

--- app/src/main/java/com/gitlab/giwiniswut/rwremount/MainActivity.java
+++ app/src/main/java/com/gitlab/giwiniswut/rwremount/MainActivity.java
@@ -2,16 +2,65 @@ package com.gitlab.giwiniswut.rwremount;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.LinearLayout;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class MainActivity extends Activity {
+    private static final String LOG_TAG = "RWRemount";
+    private boolean mRootGranted = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LinearLayout.LayoutParams mainLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-        TextView text = new TextView(this);
-        text.setText("All set!");
-        setContentView(text, mainLayoutParams);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onRequestRootButtonClick(View button) {
+        mRootGranted = requestRoot();
+
+        TextView statusView = findViewById(R.id.textView_rootStatus);
+        if (mRootGranted) {
+            statusView.setText(R.string.root_status_granted);
+            statusView.setBackgroundColor(0xFF00FF00);
+        } else {
+            statusView.setText(R.string.root_status_denied);
+            statusView.setBackgroundColor(0xFFFF0000);
+        }
+    }
+
+    private boolean requestRoot() {
+        boolean success = false;
+        try {
+            Log.d(LOG_TAG, "Requesting root access...");
+            Process p = Runtime.getRuntime().exec("su");
+            DataOutputStream terminal = new DataOutputStream(p.getOutputStream());
+            terminal.writeBytes("exit\n");
+            terminal.flush();
+            terminal.close();
+            p.waitFor();
+            int suExitValue = p.exitValue();
+            success = (suExitValue == 0);
+            if (success) {
+                Log.d(LOG_TAG, "Root access was granted!");
+            } else {
+                // Grab output of stderr and print to log
+                Scanner scanner = new Scanner(p.getErrorStream()).useDelimiter("\\A");
+                String suError = scanner.hasNext() ? "\n" + scanner.next() : "";
+                scanner.close();
+                Log.d(LOG_TAG, "ERROR: root request exited with code " + suExitValue + "\n" +
+                        suError);
+            }
+            p.destroy();
+        } catch (IOException e) {
+            Log.d(LOG_TAG, "ERROR: IOException during execution.\n" + e.getMessage());
+        } catch (InterruptedException e) {
+            Log.d(LOG_TAG, "ERROR: Process interrupted during execution.\n" + e.getMessage());
+        }
+        return success;
     }
 }
